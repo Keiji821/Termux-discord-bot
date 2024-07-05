a26f2b47887cd5e2454be6eb80647ac580609725
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-04 21:27:13 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -173,9 +173,9 @@ console.clear(); // Limpiar la consola
 console.log('[45m[37m          Termux Discord Bot          [0m');
 console.log('[32m☆ Hecho por: Keiji821[0m');
 console.log('[36m「⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎫[0m');
-console.log('[32m｜[1] Iniciar bot.          ｜[0m');
-console.log('[36m｜[2] Actualizar            ｜[0m');
-console.log('[33m｜[3] Instalar dependencias｜[0m');
+console.log('[32m｜[1] Iniciar bot           ｜[0m');
+console.log('[36m｜[2] Actualizar             ｜[0m');
+console.log('[33m｜[3] Instalar dependencias  ｜[0m');
 console.log('[31m｜[4] Salir                  ｜[0m');
 console.log('[36m ﹂⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽｣[0m');
 rl.setPrompt(`Opción: `); // Establecer el texto de la casilla "Opción: "
