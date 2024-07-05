401a9fcb89efd6ed4e25234edd44490dc5b84e10
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-04 20:20:20 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ process.stdout.write(`[48;2;35;40;40m`); // Fondo del menú en el color del Dis
 console.log(`[44m[30m          Termux Discord Bot          [0m`); // Banner centrado y destacado
 console.log('');
 console.log(`[34mHecho por: Keiji821 [0m`);
-console.log(` [34「⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺[0m`);
+console.log(` [34m「⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺[0m`);
 console.log(` [35m｜[1] Iniciar bot[0m`);
 console.log(` [36m｜[2] Actualizar[0m`);
 console.log(` [37m｜[3] Instalar dependencias[0m`);
