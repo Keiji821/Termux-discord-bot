f8b3cbe723d9718a81a24da39181baf7994d6c6c
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-05 12:55:49 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -170,15 +170,14 @@ console.error(`[31mError al instalar dependencias: ${error}[0m`);
 const showMenu = () => {
 console.clear(); // Limpiar la consola
 console.log('[45m[37m          Termux Discord Bot          [0m');
-console.log('[32m｜🪐 Hecho por: Keiji821     
-｜[0m');
-console.log('[36m｜⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺｜[0m');
+console.log('[32m｜🪐 Hecho por: Keiji821[0m');
+console.log('[36m⸂⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⸃[0m');
 console.log('[32m｜[1] Iniciar bot            ｜[0m');
 console.log('[36m｜[2] Actualizar             ｜[0m');
 console.log('[33m｜[3] Instalar dependencias  ｜[0m');
 console.log('[31m｜[4] Salir                  ｜[0m');
-console.log('[36m｜⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎭[0m');
-rl.setPrompt(`[32m｜Opción: `); // Establecer el texto de la casilla "Opción: "
+console.log('[36m⎩⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎭[0m');
+rl.setPrompt(`[32m｜Opción➤ `); // Establecer el texto de la casilla "Opción: "
 rl.prompt(); // Mostrar la casilla "Opción:"
 };
 
