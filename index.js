915a5ddebd6836ec6f4da4b49048db55b7f62b9c
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-04 21:03:45 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -172,12 +172,12 @@ const showMenu = () => {
 console.clear(); // Limpiar la consola
 console.log('[45m[37m          Termux Discord Bot          [0m');
 console.log('[32mHecho por: Keiji821[0m');
-console.log('[36m「⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺」[0m');
-console.log('[32m｜[1] Iniciar bot[0m');
-console.log('[36m｜[2] Actualizar[0m');
-console.log('[33m｜[3] Instalar dependencias[0m');
-console.log('[31m｜[4] Salir[0m');
-console.log('[36m ﹂⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻⎻[0m');
+console.log('[36m「⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎫[0m');
+console.log('[32m｜[1] Iniciar bot｜[0m');
+console.log('[36m｜[2] Actualizar｜[0m');
+console.log('[33m｜[3] Instalar dependencias｜[0m');
+console.log('[31m｜[4] Salir｜[0m');
+console.log('[36m ﹂⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽｣[0m');
 rl.setPrompt(`Opción: `); // Establecer el texto de la casilla "Opción: "
 rl.prompt(); // Mostrar la casilla "Opción:"
 };
