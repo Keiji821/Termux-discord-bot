4b4b3b0dbebf977a9e459497b8688a7db5423f90
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-04 22:45:45 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ console.log('[36m｜[2] Actualizar             ｜[0m');
 console.log('[33m｜[3] Instalar dependencias  ｜[0m');
 console.log('[31m｜[4] Salir                  ｜[0m');
 console.log('[36m ﹂⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎭[0m');
-rl.setPrompt(`Opción: `); // Establecer el texto de la casilla "Opción: "
+rl.setPrompt(`>>> `); // Establecer el texto de la casilla "Opción: "
 rl.prompt(); // Mostrar la casilla "Opción:"
 };
 
