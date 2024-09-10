1145878bb36991199b1b537977b211654c03b99e
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-09-10 01:27:59 -0400

    Actualizar gen.js

--- comandos/cc chk/herramientas/gen.js
+++ comandos/cc chk/herramientas/gen.js
@@ -1,35 +1,31 @@
 const Discord = require('discord.js');
-const https = require('https');
+const axios = require('axios');
 
 module.exports = {
-  name: 'gen',
-  description: 'Genera tarjetas de crédito aleatorias',
-  execute(message, args, client) {
-    const bin = args[0];
-    https.get(`https://binchk-api.vercel.app/bin=${bin}`, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        const json = JSON.parse(data);
-        const bank = json.bank.name;
-        const country = json.country.name;
+name: 'gen',
+description: 'Genera tarjetas de crédito aleatorias',
+execute(message, args, client) {
+const bin = args[0];
+axios.get(`https://binchk-api.vercel.app/bin=${bin}`)
+ .then(response => {
+    const json = response.data;
+    const bank = json.bank.name;
+    const country = json.country.name;
 
-        const namso = require('namso-cc-gen');
-        let res = namso.gen({
-          ShowCCV: true,
-          ShowExpDate: true,
-          ShowBank: false,
-          Month: "01",
-          Year: "2022",
-          Quantity: "10", // Generar 10 tarjetas
-          Bin: bin,
-          Format: "PIPE"
-        });
+    const namso = require('namso-cc-gen');
+    let res = namso.gen({
+      ShowCCV: true,
+      ShowExpDate: true,
+      ShowBank: false,
+      Month: "01",
+      Year: "2022",
+      Quantity: "10", // Generar 10 tarjetas
+      Bin: bin,
+      Format: "PIPE"
+    });
 
-        const cards = res.map((card, index) => {
-          return `**Tarjeta ${index + 1}**
+    const cards = res.map((card, index) => {
+      return `**Tarjeta ${index + 1}**
 
 Número de tarjeta: ${card.number}
 
@@ -40,16 +36,18 @@ Fecha de expiración: ${card.exp.month}/${card.exp.year}
 Banco: ${bank}
 País: ${country}
 `;
-        });
-
-        const cardEmbed = new Discord.EmbedBuilder()
-.setTitle("Tarjetas de Crédito Generadas")
-.setDescription(cards.join("
-"))
-.setColor("#0099ff");
-
-        message.channel.send({ embeds: [cardEmbed] });
-      });
     });
-  }
+
+    const cardEmbed = new Discord.EmbedBuilder()
+     .setTitle("Tarjetas de Crédito Generadas")
+     .setDescription(cards.join("
+"))
+     .setColor("#0099ff");
+
+    message.channel.send({ embeds: [cardEmbed] });
+  })
+ .catch(error => {
+    console.error(error);
+  });
+}
 };
