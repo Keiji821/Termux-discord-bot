eeb2aa63acc8b26d1dd50f70ef205adc8211f489
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-09-10 16:52:42 -0400

    Actualizar gen.js

--- comandos/cc chk/herramientas/gen.js
+++ comandos/cc chk/herramientas/gen.js
@@ -5,33 +5,58 @@ module.exports = {
 name: 'gen',
 description: 'Genera tarjetas de crédito aleatorias',
 execute(message, args, client) {
-const bin = args[0];
-const año = args[1];
-const mes = args[2];
-const ccv = args[3];
+const input = args.join(' ');
+const parts = input.split('|');
+
+let bin, year, month, ccv;
+
+if (parts.length === 1) {
+bin = parts[0];
+year = '2022';
+month = '01';
+ccv = 'rnd';
+} else if (parts.length === 4) {
+bin = parts[0];
+year = parts[2];
+month = parts[1];
+ccv = parts[3];
+} else {
+return message.channel.send('Error: Formato de entrada incorrecto. Uso:,gen <bin> o,gen <bin>|<mes>|<año>|<ccv>');
+}
+
 axios.get(`https://binchk-api.vercel.app/bin=${bin}`)
 .then(response => {
 const json = response.data;
-let bank = json.bank? json.bank.name : "Desconocido";
-let country = json.country? json.country.name : "Desconocido";
+const bank = json.bank? json.bank.name : "Desconocido";
+const country = json.country? json.country.name : "Desconocido";
 
+generateCards(year, month, bin, ccv, bank, country, message);
+})
+.catch(error => {
+console.error('Error al obtener datos de la API:', error);
+message.channel.send('Error al generar tarjetas de crédito. Intente nuevamente más tarde.');
+});
+}
+};
+
+function generateCards(year, month, bin, ccv, bank, country, message) {
 const namso = require('namso-cc-gen');
-let res = namso.gen({
+const res = namso.gen({
 ShowCCV: true,
 ShowExpDate: true,
 ShowBank: false,
-Month: mes,
-Year: año,
+Month: month,
+Year: year,
 Quantity: "10",
 Bin: bin,
 Format: "PIPE"
 });
 
-let cards = res.split("|"); // Divide la cadena en un array de tarjetas
+const cards = res.split("|"); // Divide la cadena en un array de tarjetas
 
 if (cards.length > 0) {
 const cardEmbed = new Discord.EmbedBuilder()
-.setTitle("Tarjetas de Crédito Generadas")
+.setTitle("Tarjetas de Crédito Generadas");
 
 let fields = [];
 
@@ -42,18 +67,12 @@ if (country) {
 fields.push({ name: "País", value: country, inline: true });
 }
 
-cardEmbed.addFields(fields)
-
-cardEmbed.setDescription(cards.join("|"))
+cardEmbed.addFields(fields);
+cardEmbed.setDescription(cards.join("|"));
 cardEmbed.setColor("#0099ff");
 
 message.channel.send({ embeds: [cardEmbed] });
 } else {
 message.channel.send("No se pudieron generar las tarjetas de crédito.");
 }
-})
-.catch(error => {
-console.error(error);
-});
 }
-};
