cd0efbebde6524e9e734431143b1f2cf3d41206c
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-09-10 02:41:36 -0400

    Actualizar gen.js

--- comandos/cc chk/herramientas/gen.js
+++ comandos/cc chk/herramientas/gen.js
@@ -6,6 +6,10 @@ name: 'gen',
 description: 'Genera tarjetas de crédito aleatorias',
 execute(message, args, client) {
 const bin = args[0];
+const month = args[1];
+const year = args[2];
+const ccv = args[3];
+
 axios.get(`https://binchk-api.vercel.app/bin=${bin}`)
 .then(response => {
 const json = response.data;
@@ -17,25 +21,31 @@ let res = namso.gen({
 ShowCCV: true,
 ShowExpDate: true,
 ShowBank: false,
-Month: "01",
-Year: "2022",
+Month: month,
+Year: year,
 Quantity: "10", // Generar 10 tarjetas
 Bin: bin,
-Format: "PIPE"
+Format: "PIPE",
+CCV: ccv
 });
 
-let cards = res.split("|"); // Divide la cadena en un array de tarjetas
+let cards = res.split("|");
 
-if (cards.length > 0) {
 const cardEmbed = new Discord.EmbedBuilder()
 .setTitle("Tarjetas de Crédito Generadas")
-.setDescription(cards.join(", "))
+.addField("Banco", bank, true)
+.addField("País", country, true)
+.setDescription(cards.map((card, index) => {
+return `**Tarjeta ${index + 1}**
+Número de tarjeta: ${card}
+Fecha de expiración: ${month}/${year}
+CCV: ${ccv}`;
+}).join("
+
+"))
 .setColor("#0099ff");
 
 message.channel.send({ embeds: [cardEmbed] });
-} else {
-message.channel.send("No se pudieron generar las tarjetas de crédito.");
-}
 })
 .catch(error => {
 console.error(error);
