ccdca3009c29719534b32321e96b0b4db61ea00e
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-09-10 02:18:38 -0400

    Actualizar gen.js

--- comandos/cc chk/herramientas/gen.js
+++ comandos/cc chk/herramientas/gen.js
@@ -7,26 +7,28 @@ description: 'Genera tarjetas de crédito aleatorias',
 execute(message, args, client) {
 const bin = args[0];
 axios.get(`https://binchk-api.vercel.app/bin=${bin}`)
- .then(response => {
-    const json = response.data;
-    const bank = json.bank.name;
-    const country = json.country.name;
+.then(response => {
+const json = response.data;
+const bank = json.bank.name;
+const country = json.country.name;
 
-    const namso = require('namso-cc-gen');
-    let res = namso.gen({
-      ShowCCV: true,
-      ShowExpDate: true,
-      ShowBank: false,
-      Month: "01",
-      Year: "2022",
-      Quantity: "10", // Generar 10 tarjetas
-      Bin: bin,
-      Format: "PIPE"
-    });
+const namso = require('namso-cc-gen');
+let res = namso.gen({
+ShowCCV: true,
+ShowExpDate: true,
+ShowBank: false,
+Month: "01",
+Year: "2022",
+Quantity: "10", // Generar 10 tarjetas
+Bin: bin,
+Format: "PIPE"
+});
 
+let cards = []; // Declarar la variable cards aquí
 
-  if (res && res.cards && Array.isArray(res.cards)) {
-  const cards = res.cards.map((card, index) => {    return `**Tarjeta ${index + 1}**
+if (res && res.cards && Array.isArray(res.cards)) {
+cards = res.cards.map((card, index) => {
+return `**Tarjeta ${index + 1}**
 
 Número de tarjeta: ${card.number}
 
@@ -37,22 +39,20 @@ Fecha de expiración: ${card.exp.month}/${card.exp.year}
 Banco: ${bank}
 País: ${country}
 `;
-    });
-
-  });
+});
 } else {
-  console.error('No se pudo generar las tarjetas');
+console.error('No se pudo generar las tarjetas');
 }
 
-    const cardEmbed = new Discord.EmbedBuilder()
+const cardEmbed = new Discord.EmbedBuilder()
 .setTitle("Tarjetas de Crédito Generadas")
 .setDescription(cards.join(", "))
 .setColor("#0099ff");
 
-    message.channel.send({ embeds: [cardEmbed] });
-  })
- .catch(error => {
-    console.error(error);
-  });
+message.channel.send({ embeds: [cardEmbed] });
+})
+.catch(error => {
+console.error(error);
+});
 }
 };
