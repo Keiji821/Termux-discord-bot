3180fed4d2df242d08b7a183aa00bbc669abbeb3
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-09-10 02:25:12 -0400

    Actualizar gen.js

--- comandos/cc chk/herramientas/gen.js
+++ comandos/cc chk/herramientas/gen.js
@@ -2,33 +2,33 @@ const Discord = require('discord.js');
 const axios = require('axios');
 
 module.exports = {
-name: 'gen',
-description: 'Genera tarjetas de crédito aleatorias',
-execute(message, args, client) {
-const bin = args[0];
-axios.get(`https://binchk-api.vercel.app/bin=${bin}`)
-.then(response => {
-const json = response.data;
-const bank = json.bank.name;
-const country = json.country.name;
+  name: 'gen',
+  description: 'Genera tarjetas de crédito aleatorias',
+  execute(message, args, client) {
+    const bin = args[0];
+    axios.get(`https://binchk-api.vercel.app/bin=${bin}`)
+     .then(response => {
+        const json = response.data;
+        const bank = json.bank.name;
+        const country = json.country.name;
 
-const namso = require('namso-cc-gen');
-let res = namso.gen({
-ShowCCV: true,
-ShowExpDate: true,
-ShowBank: false,
-Month: "01",
-Year: "2022",
-Quantity: "10", // Generar 10 tarjetas
-Bin: bin,
-Format: "PIPE"
-});
+        const namso = require('namso-cc-gen');
+        let res = namso.gen({
+          ShowCCV: true,
+          ShowExpDate: true,
+          ShowBank: false,
+          Month: "01",
+          Year: "2022",
+          Quantity: "10", // Generar 10 tarjetas
+          Bin: bin,
+          Format: "PIPE"
+        });
 
-let cards = []; // Declarar la variable cards aquí
+        let cards = []; // Declarar la variable cards aquí
 
-if (res && res.cards && Array.isArray(res.cards)) {
-cards = res.cards.map((card, index) => {
-return `**Tarjeta ${index + 1}**
+        if (res && res.cards && Array.isArray(res.cards)) {
+          cards = res.cards.map((card, index) => {
+            return `**Tarjeta ${index + 1}**
 
 Número de tarjeta: ${card.number}
 
@@ -39,18 +39,24 @@ Fecha de expiración: ${card.exp.month}/${card.exp.year}
 Banco: ${bank}
 País: ${country}
 `;
-});
-} else {
-console.error('No se pudo generar las tarjetas');
-}
+          });
+        } else {
+          console.error('No se pudo generar las tarjetas');
+        }
 
-if (cards.length > 0) {
-const cardEmbed = new Discord.EmbedBuilder()
-.setTitle("Tarjetas de Crédito Generadas")
-.setDescription(cards.join(", "))
-.setColor("#0099ff");
+        if (cards.length > 0) {
+          const cardEmbed = new Discord.EmbedBuilder()
+           .setTitle("Tarjetas de Crédito Generadas")
+           .setDescription(cards.join(", "))
+           .setColor("#0099ff");
 
-message.channel.send({ embeds: [cardEmbed] });
-} else {
-message.channel.send("No se pudieron generar las tarjetas de crédito.");
-}
+          message.channel.send({ embeds: [cardEmbed] });
+        } else {
+          message.channel.send("No se pudieron generar las tarjetas de crédito.");
+        }
+      })
+     .catch(error => {
+        console.error(error);
+      });
+  }
+};
