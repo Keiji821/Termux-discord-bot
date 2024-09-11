289b069be1cdfb55cc8cc74951ad7531a740cb47
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-09-10 21:36:27 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -193,14 +193,14 @@ lolcatjs.fromString(
 );
 
 lolcatjs.fromString('    Hecho por: Keiji821');
-lolcatjs.fromString();
+lolcatjs.fromString('');
 lolcatjs.fromString('[1] Iniciar bot');
 lolcatjs.fromString('[2] Actualizar');            
 lolcatjs.fromString('[3] Instalar dependencias');
 lolcatjs.fromString('[4] Salir');
-lolcatjs.fromString();
+lolcatjs.fromString(' ');
 rl.setPrompt(`[32m[1m  ➤ `);
-lolcatjs.fromString();
+lolcatjs.fromString(' ');
 rl.prompt(); 
 };
 
